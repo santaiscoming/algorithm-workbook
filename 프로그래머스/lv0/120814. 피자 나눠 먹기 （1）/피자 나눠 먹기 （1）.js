908543011c75789f3f6b4d35ce08d95d51fa8fe7
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-07 06:41:12 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    var answer = 1;
-    while(n > 0) {
-        n -= 1;
-        if (n === 0) break;
-        if (n % 7 === 0) answer += 1;
-    }
-    return answer;
+    // var answer = 1;
+    // while(n > 0) {
+    //     n -= 1;
+    //     if (n === 0) break;
+    //     if (n % 7 === 0) answer += 1;
+    // }
+    // return answer;
+    
+    return n % 7 === 0 ? n / 7 : Math.ceil(n / 7)
 }

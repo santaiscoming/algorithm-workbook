5f4e8a9f74235cbfcbb0025de9eaab6fcc3dddca
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-08 19:01:31 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,6 +1,8 @@
 function solution(num_list) {
-    const even = num_list.filter((num) => num % 2 === 0);
-    const odd = num_list.filter((num) => num % 2 === 1);
+//     const even = num_list.filter((num) => num % 2 === 0);
+//     const odd = num_list.filter((num) => num % 2 === 1);
     
-    return [even.length, odd.length]
+//     return [even.length, odd.length]
+    
+    return num_list.reduce((acc, cur) => (cur & 1 ? acc[1]++ : acc[0]++, acc), [0, 0])
 }

6000b9cbf9e4ef6d765f5cf19e4c50105c71cbb7
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-10 01:02:27 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,5 +1,6 @@
 function solution(my_string) {
     // pattern이 정규식이면 전역(g) 플래그가 설정되어 있어야 합니다. 그렇지 않으면 TypeError가 발생합니다.
+    // -> 전역 플래그가 없으면 첫번째로 매칭되는 패턴만 대체되기 때문
     const reg = /[aeiou]/g
     return my_string.replaceAll(reg, '');
 }

67a9fe2886f0dba564c1616d02467b9632135d39
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-09 22:50:49 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,12 @@
+const discountMap = new Map([
+  [500000, 20],
+  [300000, 10],
+  [100000, 5],
+]);
+
+function solution(price) {
+  for (let [priceRef, discountRate] of discountMap) {
+      if(price >= priceRef) return Math.floor(price - (price / 100) * discountRate)
+  }
+    return Math.floor(price)
+}

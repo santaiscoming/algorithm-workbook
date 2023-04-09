64c90f53a5e23d01b7916870b110c64391bba5f5
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-10 00:06:09 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,10 +1,14 @@
 function solution(numbers) {
-    let max = Number.MIN_SAFE_INTEGER
-    for(let i = 0; i < numbers.length; i++) {
-        for(let j = i + 1; j < numbers.length; j++) {
-            const multiplication = numbers[i] * numbers[j]
-            multiplication > max ? max = multiplication : max
-        }
-    }
-    return max
+//     O(n^2)
+    // let max = Number.MIN_SAFE_INTEGER
+    // for(let i = 0; i < numbers.length; i++) {
+    //     for(let j = i + 1; j < numbers.length; j++) {
+    //         const multiplication = numbers[i] * numbers[j]
+    //         multiplication > max ? max = multiplication : max
+    //     }
+    // }
+    // return max
+    
+    numbers.sort((a, b) => b - a)
+    return numbers[0] * numbers[1]
 }

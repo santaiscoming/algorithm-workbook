97bd16be5d6adffdc4dfd7eb3442ac3fa4bbe5d5
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-10 00:54:00 +0900

    [level 0] Title: 순서쌍의 개수, Time: 76.74 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,10 +1,4 @@
 function solution(n) {
-  const arr = [];
-  for (let i = 0; i <= n; i++) {
-    for (let j = 0; j <= n; j++) {
-      if (i * j > n) break;
-      if (i * j === n) arr.push([i, j]);
-    }
-  }
-    return arr.length
+  //약수 구하기
+    return Array.from({length : n}, (_, idx) => idx + 1).filter((num) => n % num === 0).length
 }

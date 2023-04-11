3b694945349a877098ebf83046a46470606347a4
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-12 02:20:39 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -4,4 +4,11 @@ function solution(my_string) {
                         // 각각의 문자타입의 숫자를 숫자타입으로 안바꿔주면 fail
                          .map((str) => Number(str))
                          .sort((a, b) => a - b)
+    
+    const regExp = /\d/g
+                        // match 메서드는 일치하는 문자열을 반환하고 없으면 null 반환
+                        // 그렇다는 뜻은 1,2,2,3,9 라는 값이 truthy한 값이기에 true로 평가되는것이지 실제로 true를 반환하지 않으니 !! 부정연산자를 이용해 true값으로 변환
+    return [...my_string].filter((str) => !!str.match(regExp))
+                         .map((str) => Number(str))
+                         .sort((a, b) => a - b)
 }

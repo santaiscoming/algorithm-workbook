010c4c4fcdad5a9cf6d2e6bd29fbd5bf069e1a93
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-12 05:16:53 +0900

    [level 0] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,4 +1,7 @@
 function solution(box, n) {
     return box.map((axis) => Math.floor(axis / n))
               .reduce((acc, cur) => acc * cur, 1);
+    
+    // let [x, y, z] = box
+    // return Math.floor(x / n) * Math.floor(y / n) * Math.floor(z / n)
 }

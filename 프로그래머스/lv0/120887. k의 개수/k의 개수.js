7ccb4a29890b36b7bcfccca884de1f05d8352c10
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-13 23:08:57 +0900

    [level 0] Title: k의 개수, Time: 5.90 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,7 +1,16 @@
+// function solution(i, j, k) {
+//     const nums = Array.from({length : j - i  + 1}, (_, idx) => idx + i)
+    
+//     const allNums  = nums.map((num) => [...String(num)]).flat();
+    
+//     return allNums.filter((num) => Number(num) === k).length
+// }
+
 function solution(i, j, k) {
-    const nums = Array.from({length : j - i  + 1}, (_, idx) => idx + i)
-    
-    const allNums  = nums.map((num) => [...String(num)]).flat();
-    
-    return allNums.filter((num) => Number(num) === k).length
+  let a = "";
+  for (i; i <= j; i++) {
+    a += i;
+  }
+
+  return a.split(k).length - 1;
 }

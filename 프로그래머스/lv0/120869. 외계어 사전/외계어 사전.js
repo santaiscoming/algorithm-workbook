1acadb21faa2feeed9bf38d3ead200352c9f5cd3
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-16 20:40:33 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,18 +1,3 @@
 function solution(spell, dic) {
-  // dic 배열을 돌면서 각 문자열의 출몰 빈도 계산
-  for (let str of dic) {
-    const map = [...str].reduce((acc, cur) => {
-        acc[cur] = (acc[cur] || 0) + 1
-        return acc
-    }, {});
-      
-    const strFrequency = [...Object.keys(map)];
-      
-    const spellIncludes = strFrequency.filter((char) => [...spell].includes(char))
-    
-    if(spellIncludes.length === spell.length) return 1;
-      // 출몰의 키값에 spell 만으로 형성되어있으면 count ++
-  }
-    
-    return 2;
+   return dic.some((str) => [...str].sort().join('') === spell.sort().join('')) ? 1 : 2
 }

d8d927ea69ac62cdc047fa891eae6b947b255e26
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-23 23:20:41 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,8 @@
 function solution(seoul) {
-    const idx = seoul.indexOf('Kim')
+    
+    // ES6에 추가된 findIndex(callback) 사용도 가능
+    const idx = seoul.findIndex((element) => element === 'Kim')
+    // const idx = seoul.indexOf('Kim')
     
     return `김서방은 ${idx}에 있다`
 }

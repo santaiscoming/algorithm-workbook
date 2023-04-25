d64e3cf12bf8d6e05d364497537bc789138efa98
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-25 19:10:30 +0900

    [level 2] Title: 문자열 압축, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -0,0 +1,42 @@
+const wordsSlice = (words, numberOfWord) => {
+  let count = 0;
+  const result = [];
+
+  while (true) {
+    if (count >= words.length) break;
+    result.push([...words].splice(count, numberOfWord));
+    count += numberOfWord;
+  }
+
+  return result.map(arr => arr.reduce((acc, cur) => acc + cur));
+};
+
+function solution(s) {
+  const compress = [];
+
+  if(s.length === 1) return 1
+  for (let i = 1; i <= s.length / 2; i++) {
+    // map의 사이즈가 작은걸 계속 업데이트 후에 i값 Math.min?
+    const slice = wordsSlice(s, i);
+    let count = 1;
+    const innercomp = [];
+
+    for (let i = 1; i <= slice.length; i++) {
+      if (slice[i] === slice[i - 1]) {
+        count += 1;
+        continue;
+      } else {
+        if (count > 1) {
+          innercomp.push(count);
+          innercomp.push(slice[i - 1]);
+          count = 1;
+          continue;
+        }
+        innercomp.push(slice[i - 1]);
+      }
+    }
+    compress.push(innercomp.join(''));
+  }
+
+  return Math.min(...compress.map(str => str.length));
+}

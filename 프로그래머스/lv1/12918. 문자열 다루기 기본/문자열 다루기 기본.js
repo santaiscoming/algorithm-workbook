147837b4181f223a0ef153060e07115f562f1104
백현영 <116708517+daal2quf@users.noreply.github.com>
2023-04-26 12:37:28 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -3,4 +3,8 @@ function solution(s) {
     if(s.length !== 4 && s.length !== 6) return false
     // 숫자가 아닌것을 찾아서 배열로 반환
     return !Array.isArray(s.match(/\D/g))
+    
+    // 원했던 풀이
+//   let regex = /^\d{6}$|^\d{4}$/;
+//   return regex.test(s);
 }

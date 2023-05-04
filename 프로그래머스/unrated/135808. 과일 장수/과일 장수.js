99546898bf8ac68d5159aab44010ae7db8fb20d3
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-05-04 10:15:33 +0900

    [unrated] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,19 @@
 function solution(k, m, score) {
-    // 사과 점수 정렬
-    // 박스갯수만큼 반복
+    // m : 한상자에 들어갈 사과 개수
+    // p : 가장 낮은 점수 사과
+    // k : 가장 높은 점수 사과
+    // 사과 한상자 가격 : p * m
+    
+    // 내림차순으로 정렬후 m개씩 넣어준다 -> arr
+    // 해당 상자의 가격은 최솟값(Math.min(arr)) * 한상자에 들어간 사과 개수 (m)
+        // if 상자.length < m : 못팜
     let result = 0;
-    score.sort((a, b) => b-a);   
-
-    for(let i = 1; i <= Math.floor(score.length / m); i++) {
-        result += score[m * i - 1] * m;
+    score.sort((a, b) => b - a)
+    
+    for(let i = m-1; i < score.length; i += m){
+        result += score[i] * m;
     }
-    return  result;
+
+    
+    return result
 }

f115af6244018fc37e15c3a37aa234b7c980d6d8
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-05-04 11:52:39 +0900

    [unrated] Title: 덧칠하기, Time: 1.73 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,15 @@
+function solution(n, m, section) {
+    let answer = 0;
+
+    let complete = 0;
+
+    section.forEach((wall) => {
+        if(complete > n) return answer
+        if (wall > complete) {
+           complete = wall + m - 1;
+           answer += 1;
+         }
+    });
+
+  return answer;
+}

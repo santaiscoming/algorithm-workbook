e19bfea1946529db0431f2307948703ba686709c
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-05-11 12:06:52 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,19 @@
+
+
+function solution(ingredient) {
+    let answer = 0;
+    const bugger = [];
+
+    for (let i = 0; i < ingredient.length; i++) {
+        bugger.push(ingredient[i]);
+        if (bugger.length >= 4) {
+        if (bugger[bugger.length - 4] === 1 && bugger[bugger.length - 3] === 2 && bugger[bugger.length - 2] === 3 && bugger[bugger.length - 1] === 1) {
+            Array.from({length: 4}, () => bugger.pop())
+
+            answer++;
+            }
+        }
+    }
+
+    return answer;
+}

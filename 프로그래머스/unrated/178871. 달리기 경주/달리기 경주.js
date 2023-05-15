a6837115f958e9737cb01d2b833b839c5feec4ac
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-05-15 10:02:45 +0900

    [unrated] Title: 달리기 경주, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,27 @@
+function solution(players, callings) {
+    let playerMap = players.reduce((map, curr, idx) => {
+        map[curr] = idx + 1
+        return map
+    }, {})
+    
+    callings.forEach((passPlayer, idx) => {
+        let findRank;
+        findRank = playerMap[passPlayer]
+        let nextPlayer = players[findRank - 2]
+        players[findRank - 2] = passPlayer
+        players[findRank - 1] = nextPlayer
+        playerMap[passPlayer] -= 1;
+        playerMap[nextPlayer] += 1;
+    })
+    
+    // callings.forEach((passPlayer, idx) => {
+    //     let findRank;
+    //     findRank = playerMap[passPlayer]
+    //     let nextPlayer = players[findRank - 2]
+    //     playerMap[passPlayer] -= 1;
+    //     playerMap[nextPlayer] += 1;
+    //     players = Object.keys(Object.fromEntries(Object.entries(playerMap).sort((a, b) => a[1] - b[1])))
+    // })
+    
+    return players
+}

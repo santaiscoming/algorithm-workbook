d4ba2488e05744655098c522bdbd96623073f74e
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-05-19 14:15:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.04 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,27 @@
+// 0을 제거하고 제거한 0의 개수륿 반환하는 함수
+const removeZero = (num) => {
+        let strArr = [...String(num)].map((char) => Number(char))
+        let zeroCount = 0;
+        let result = ''
+        for(let i = 0; i < strArr.length; i++) {
+            if(!strArr[i]) zeroCount++;
+            if(strArr[i]) result += strArr[i]
+        }
+        return [zeroCount, result]
+    }
+
+function solution(s) {
+    let zeroCount = 0;
+    let convertBinaryCount = 0
+    
+    // 0을 제거한 길이에 대해 이진 변환 하는 로직
+    while(true) {
+        let [count, removeZeroNum] = removeZero(s)
+        zeroCount += count;
+        s = (removeZeroNum.length).toString(2)
+        convertBinaryCount++;
+        if(s == 1) break
+    }
+    
+    return [convertBinaryCount, zeroCount]
+}

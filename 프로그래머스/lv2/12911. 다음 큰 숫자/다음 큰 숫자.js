38bdd39ed11d949cba07f90ac0d52e6df709d9d4
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-06-06 13:27:43 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,24 +1,44 @@
+// const countOnes = (binaryNum) => {
+//   let count = 0;
+//   for (let i = 0; i < binaryNum.length; i++) {
+//     if (binaryNum[i] === '1') {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+
+// function solution(n) {
+//   let compareNum = n + 1;
+//   let defaultOnesCount = countOnes(n.toString(2));
+
+//   while (true) {
+//     let compareNumCount = countOnes(compareNum.toString(2));
+
+//     if (defaultOnesCount === compareNumCount) {
+//       return compareNum;
+//     }
+
+//     compareNum++;
+//   }
+// }
+
+
+
 function solution(n) {
-  let compareNum = n + 1;
-  let sameOneCount = countOnes(n.toString(2));
+    let compareNum = n + 1
+    let binaryNum = n.toString(2)
+    
+    let sameOneCount = [...String(binaryNum)].filter((num) => Number(num)).length
 
-  while (true) {
-    let compareNumCount = countOnes(compareNum.toString(2));
-
-    if (sameOneCount === compareNumCount) {
-      return compareNum;
+    while(true) {
+        let compareBinaryNum = compareNum.toString(2)
+        let compareNumCount = [...String(compareBinaryNum)].filter((num) => Number(num)).length
+        
+     
+        if(sameOneCount === compareNumCount) return compareNum
+        
+           compareNum++;
     }
-
-    compareNum++;
-  }
-}
-
-function countOnes(binaryNum) {
-  let count = 0;
-  for (let i = 0; i < binaryNum.length; i++) {
-    if (binaryNum[i] === '1') {
-      count++;
-    }
-  }
-  return count;
 }

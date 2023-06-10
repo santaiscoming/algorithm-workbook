1c7e51d5197c6f0d54d403428139d863050d60d1
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-06-11 01:39:20 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.29 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,14 +1,33 @@
 function solution(numer1, denom1, numer2, denom2) {
-    // 손코딩의 위대함
-  let denom = numer1 * denom2 + numer2 * denom1;
-  let numer = denom1 * denom2;
-  let sameMax = 1;
-
-    // 최대공약수
-  for (let i = 1; i <= denom; i++) {
-    if (denom % i === 0 && numer % i === 0) {
-      sameMax = i;
+  let ans = [];
+  let maxCommon;
+  let 분자 = denom1 * numer2 + denom2 * numer1;
+  ans.push(분자);
+  let 분모 = denom1 * denom2;
+  ans.push(분모); //8
+    // 최대공약수를 구해보자 -> 분자와 분모의
+     let ref = Math.max(분자, 분모)
+    for(let i = ref; i > 0; i--) {
+        // console.log({i, 분자, 분모})
+        if(분자 % i === 0 && 분모 % i === 0) {
+            maxCommon = i
+            break;
+        } 
     }
-  }
-  return [denom / sameMax, numer / sameMax];
+    
+    return [분자 / maxCommon, 분모 / maxCommon]
+    
+    
+  // //만약에 분자랑 분모가 계속 2로 나눌수 있다면 2로 나누어라
+  // if (분자 % minMultiple === 0 && 분모 % minMultiple === 0) {
+  //   분자 = 분자 / minMultiple;
+  //   분모 = 분모 / minMultiple;
+  //   let ans = [];
+  //   ans.push(분자);
+  //   ans.push(분모);
+  //   console.log(분자, 분모);
+  //   return ans;
+  // }
+  // console.log(ans);
+  // return ans;
 }

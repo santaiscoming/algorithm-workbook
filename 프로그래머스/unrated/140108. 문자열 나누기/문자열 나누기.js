4497e6c3859ec075a2893149d53bf52966617692
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-06-14 17:25:23 +0900

    [unrated] Title: 문자열 나누기, Time: 3.33 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -10,21 +10,22 @@ function solution(s) {
   let firstChar = ''
 
   for (let i = 0; i < s.length; i++) {
-      console.log(firstChar)
-    if (!firstChar) firstChar = s[i]
-    if (firstChar === s[i]) {
-      sameCount += 1
-    } else {
-      notSameCount += 1
-    }
+    let curChar = s[i]
+      // 우리가 돌아야할 문자의 0번째를 이니셜라이즈
+    if (!firstChar) firstChar = curChar
+    // 처음등장한 문자와 s[i] 가 같으면
+    if (firstChar === curChar) sameCount += 1
+    // 처음등장한 문자와 s[i] 가 같지않으면
+    if (firstChar !== curChar) notSameCount += 1
+    //  x와 x가 아닌 다른 글자들이 나온 횟수를 각각 셉니다. 처음으로 두 횟수가 같아지는 순간 멈추고, 지금까지 읽은 문자열을 분리합니다.
     if (sameCount === notSameCount) {
       result += 1
       sameCount = 0
       notSameCount = 0
       firstChar = ''
     }
-      console.log(firstChar)
   }
+     
   if(firstChar) result += 1;
   return result
 }

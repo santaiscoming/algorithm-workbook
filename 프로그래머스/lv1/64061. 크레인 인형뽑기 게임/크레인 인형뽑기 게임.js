b61ed2d9e820b406094dc3a5537a0eda658cc3b2
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-06-15 18:08:34 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,59 +1,29 @@
-// 1. moves를 돌면서 해당 위치에서 0이 아닌값을 찾습니다.
-
-// 2-1   1에서 찾은값과 stack의 가장 마지막값이 같은경우 cnt해주고 stack에서 삭제합니다.
-// 2-2   1에서 찾은값과 stack의 가장 마지막값이 다른경우 stack에 push합니다.
-// 2-3   1에서 찾은값을 0으로 바꿔줍니다. 
-
-// 3. 총 cnt한 개수 * 2 를 출력해줍니다.
-
-
-// [ -> [ y : 4, x : 0 ]
-//   [0, 0, 0, 0, 0],
-//   [0, 0, 1, 0, 3],
-//   [0, 2, 5, 0, 1],
-//   [4, 2, 4, 4, 2],
-//   [3, 5, 1, 3, 1],
-// ];
-
-
-// 1번부터 moves : 1 5 3 5 1 2 1 4 
-// 0번부터 moves : 0 4 2 4 0 1 0 3 
-
-// [
-//   [0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0],
-//   [0, 0, 5, 0, 0],
-//   [0, 2, 4, 0, 2],
-//   [0, 5, 1, 3, 1],
-// ];
-
-// 4311324
-// 424
-// 11, 33 => 총 4개 
-
 function solution(board, moves) {
-    let stack = [];
-    let cnt = 0;
-
-        
-    moves.forEach((item)=>{
-        let cur = item-1;
-        
-        for(let j=0; j<board.length; j++){
-            let pick = board[j][cur];
-            
-            if(pick!==0){
-                if(stack[stack.length-1] === pick){
-                    stack.pop();
-                    cnt++;  
-                }else{
-                    stack.push(pick);
-                }
-                
-                board[j][cur] = 0;
-                break;
-            }
+    // 행렬을 뒤집고 move[i] 의 행렬에서 0이 아닐떄까지 들어가자
+    const newMap = Array.from({length : board.length}, () => [])
+    const result = [];
+    let count = 0;
+    
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j <board.length; j++) {
+            if(board[j][i]) newMap[i].push(board[j][i])
         }
+    }
+    console.log(newMap)
+    moves.forEach((move, idx) => {
+        let resultLast = result[result.length - 1]
+        let curKakao = newMap[move - 1].shift()
+        if(!curKakao) return
+
+        // console.log(`mve : ${move}, last : ${resultLast}, cur : ${curKakao}, result : ${result}`)
+        if(curKakao === resultLast) {
+            count += 2;
+            result.splice(-1)
+        } else {
+            result.push(curKakao)
+        }
+
     })
-    return cnt*2;
+    
+    return count
 }

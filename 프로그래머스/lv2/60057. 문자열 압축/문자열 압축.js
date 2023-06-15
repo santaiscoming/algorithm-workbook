86cebabb06d2e36e1c28057b6348de45b0461cd7
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-06-15 20:05:51 +0900

    [level 2] Title: 문자열 압축, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -1,41 +1,28 @@
-const wordsSlice = (words, numberOfWord) => {
-  let count = 0;
-  const result = [];
-
-  while (true) {
-    if (count >= words.length) break;
-    result.push([...words].splice(count, numberOfWord));
-    count += numberOfWord;
-  }
-
-  return result.map(arr => arr.reduce((acc, cur) => acc + cur));
-};
-
 function solution(s) {
-  if (s.length === 1) return 1;
+    const result = [];
+    const limitCount = Math.floor(s.length / 2)
+    // 예외처리
+    if (s.length == 1) return 1;
+    
+    for (var i = 1; i <= limitCount; i++) {
+        var b = '';
+        var cnt = 1;
+        var currentStr = s.slice(0, i);
 
-  const compress = [];
-  for (let i = 1; i <= s.length / 2; i++) {
-    // map의 사이즈가 작은걸 계속 업데이트 후에 i값 Math.min?
-    const slice = wordsSlice(s, i);
-    let count = 1;
-    const innercomp = [];
-
-    for (let i = 1; i <= slice.length; i++) {
-      if (slice[i] === slice[i - 1]) {
-        count += 1;
-        continue;
-      }
-      if (count > 1) {
-        innercomp.push(count);
-        innercomp.push(slice[i - 1]);
-        count = 1;
-        continue;
-      }
-      innercomp.push(slice[i - 1]);
+        for (var j = i; j < s.length + i; j += i) {
+            let nextStr = s.slice(j, i+j);
+            if (currentStr === nextStr) {
+                cnt++;
+            } else {
+                if (cnt != 1) b = b + String(cnt) + currentStr;
+                else b = b + currentStr;
+                
+                currentStr = nextStr;
+                cnt = 1;
+            }
+        }
+        result.push(b.length);
     }
-    compress.push(innercomp.join(''));
-  }
 
-  return Math.min(...compress.map(str => str.length));
+    return Math.min(...result);
 }

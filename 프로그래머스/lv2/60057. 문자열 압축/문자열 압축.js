a82a84f3d8ee4dbf61a79a7d0beb4b3d0144fc0c
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-06-15 20:08:04 +0900

    Update 문자열 압축.js

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -4,24 +4,24 @@ function solution(s) {
     // 예외처리
     if (s.length == 1) return 1;
     
-    for (var i = 1; i <= limitCount; i++) {
-        var b = '';
-        var cnt = 1;
-        var currentStr = s.slice(0, i);
+    for (let i = 1; i <= limitCount; i++) {
+        let accStr = '';
+        let count = 1;
+        let sliceStr = s.slice(0, i);
 
-        for (var j = i; j < s.length + i; j += i) {
-            let nextStr = s.slice(j, i+j);
-            if (currentStr === nextStr) {
-                cnt++;
+        for (let j = i; j < s.length + i; j += i) {
+            let nextSliceStr = s.slice(j, i+j);
+            if (sliceStr === nextSliceStr) {
+                count++;
             } else {
-                if (cnt != 1) b = b + String(cnt) + currentStr;
-                else b = b + currentStr;
+                if (count != 1) accStr = accStr + String(count) + sliceStr;
+                else accStr = accStr + sliceStr;
                 
-                currentStr = nextStr;
-                cnt = 1;
+                sliceStr = nextSliceStr;
+                count = 1;
             }
         }
-        result.push(b.length);
+        result.push(accStr.length);
     }
 
     return Math.min(...result);

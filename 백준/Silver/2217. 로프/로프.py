777477af6b769455ea52997644274d7d8b4b8795
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-02-27 19:37:43 +0900

    [Silver IV] Title: 로프, Time: 4448 ms, Memory: 41900 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,27 @@
+import sys
+
+# sys.stdin = open("./input.txt", "r")
+
+
+def readlines(count):
+    # return list(map(lambda x: input(), range(count)))
+    return [input() for _ in range(count)]
+
+
+N = int(input())
+ropes = list(map(int, readlines(N)))
+
+
+def solution(N, ropes):
+    result = []
+    ascendingRopes = sorted(ropes, reverse=True)
+
+    for i in range(N):
+        ropeCount = i + 1
+        weight = ascendingRopes[i] * ropeCount
+        result.append(weight)
+
+    return max(result)
+
+
+print(solution(N, ropes))

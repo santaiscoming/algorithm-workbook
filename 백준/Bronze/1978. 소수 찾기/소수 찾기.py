6f0a6364c78dd892e841950f00d45d43e8113a66
백현영 <116708517+santaiscoming@users.noreply.github.com>
2024-03-22 15:23:29 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.setrecursionlimit(10**8)
+# sys.stdin = open("./input.txt", "r")  # 제거
+input = sys.stdin.readline
+
+N = int(input())
+nums = [int(num) for num in input().split(" ")]
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+print(len(list(filter(is_prime, nums))))

4d0a0b89a22a16246667615560d13c326d67694f
백현영 <116708517+santaiscoming@users.noreply.github.com>
2024-03-27 15:57:02 +0900

    Update 탑.py

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -11,6 +11,23 @@ def solution(tops, N):
     stack = []
     result = [0] * N
 
+    for curr_top_idx in range(N):
+        while stack:
+            if tops[stack[-1]] < tops[curr_top_idx]:
+                stack.pop()
+            else:
+                break
+
+        if stack:
+            result[curr_top_idx] = stack[-1] + 1
+        stack.append(curr_top_idx)
+
+    return result
+
+def solution2(tops, N):
+    stack = []
+    result = [0] * N
+
     for i in range(N):
         while stack and tops[stack[-1]] < tops[i]:
             stack.pop()

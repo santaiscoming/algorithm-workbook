84e4cf7090d1d8d3eee75e7fedde139359363738
백현영 <116708517+santaiscoming@users.noreply.github.com>
2024-08-30 01:24:33 +0900

    [Silver V] Title: 집합, Time: 2836 ms, Memory: 38852 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,32 +1,66 @@
 import sys
+from typing import Set, List
 
 # sys.stdin = open("./input.txt", "r")
 input = sys.stdin.readline
 
-
 M = int(input())
 
-bit = 0
-for i in range(M):
-    comm = input().rstrip().split()
 
-    if comm[0] == "all":
-        bit = (1 << 20) - 1
-    elif comm[0] == "empty":
-        bit = 0
+def add(bit, x):
+    return bit | (1 << x)
+
+
+def remove(bit, x):
+    return bit & ~(1 << x)
+
+
+def check(bit, x):
+    isExistBit = bit & (1 << x)
+
+    if isExistBit:
+        print(1)
     else:
-        op = comm[0]
-        num = int(comm[1]) - 1
+        print(0)
 
-        if op == "add":
-            bit |= (1 << num)
-        elif op == "remove":
-            bit &= ~(1 << num)
-        elif op == "check":
-            if bit & (1 << num) == 0:
-                print(0)
-            else:
-                print(1)
 
-        elif op == "toggle":
-            bit = bit ^ (1 << num)
+def toggle(bit, x):
+    return bit ^ (1 << x)
+
+
+def all():
+    return (1 << 21) - 1
+
+
+def empty():
+    return 0
+
+
+def solution():
+    bit = 1
+    funcs = {
+        "add": add,
+        "remove": remove,
+        "check": check,
+        "toggle": toggle,
+        "all": all,
+        "empty": empty,
+    }
+
+    for _ in range(M):
+        operator = input().split()
+        cmd, num = operator if len(operator) == 2 else (operator[0], 0)
+        num = int(num)
+
+        if cmd != "empty" and cmd != "all":
+            if cmd == "check":
+                funcs[cmd](bit, num)
+                continue
+
+            bit = funcs[cmd](bit, num)
+
+        else:
+            bit = funcs[cmd]()
+
+
+solution()

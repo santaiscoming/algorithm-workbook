7068959f3bb62dd7d2d126f2173e5d00b2157608
백현영 <116708517+santaiscoming@users.noreply.github.com>
2024-09-13 14:15:45 +0900

    [Silver III] Title: 주유소, Time: 84 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -9,30 +9,13 @@ price = list(map(int, input().split()))
 
 
 def solution(N, roads, price):
-    skip_idx = None
     spend_money = 0
-    min_price = min(price)
+    min_price = price[0]
 
-    for idx, cur_road in enumerate(roads):
-        if skip_idx == idx and skip_idx is not None:
-            continue
-
-        curr_price = price[idx]
-
-        if price[idx] == min_price:
-            rest_distance = sum(roads[idx:])
-            spend_money += min_price * rest_distance
-            break
-
-        if idx != len(roads) - 1:
-            next_price = price[idx + 1]
-
-            if next_price > curr_price:
-                next_road = roads[idx + 1]
-                spend_money += (curr_price * next_road) + (cur_road * curr_price)
-                skip_idx = idx + 1
-            else:
-                spend_money += cur_road * curr_price
+    for idx in range(len(roads)):
+        if price[idx] < min_price:
+            min_price = price[idx]
+        spend_money += min_price * roads[idx]
 
     print(spend_money)
 

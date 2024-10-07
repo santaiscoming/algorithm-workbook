c8d64311d9918f5fc913ff909317e1e139a4ebcb
백현영 <116708517+santaiscoming@users.noreply.github.com>
2024-10-07 19:54:32 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,36 +1,48 @@
 import sys
+import math
+
 
 sys.setrecursionlimit(10**8)
 # sys.stdin = open("./input.txt", "r")  # 제거
 input = sys.stdin.readline
 
+global T, nums
+
 T = int(input())
 nums = [int(input()) for i in range(T)]
 
-
-def is_prime(num):
-    if num < 2:
-        return False
-
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-
-    return True
+import sys
 
 
-def goldbach_partition(num):
-    # 두수의 가장 작은 차이 :
-    # 중앙값에서 시작해 벌어지다가 가장 먼저 만나는 값이 차이가 제일 적다
-    a, b = num // 2, num // 2
+def sieve(max_num):
+    # """에라토스테네스의 체를 이용해 소수를 미리 구함."""
+    is_prime = [True] * (max_num + 1)
+    is_prime[0], is_prime[1] = False, False
+    for i in range(2, int(max_num**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_num + 1, i):
+                is_prime[j] = False
+    return is_prime
 
-    while a > 1:
-        if is_prime(a) and is_prime(b):
-            return [a, b]
-        a -= 1
-        b += 1
+
+def find_goldbach_pair(num, is_prime):
+    # """주어진 숫자 num에 대해 골드바흐 파티션을 찾음."""
+    for i in range(num // 2, 1, -1):
+        if is_prime[i] and is_prime[num - i]:
+            return i, num - i
+    return None
 
 
-for num in nums:
-    a, b = goldbach_partition(num)
-    print(a, b)
+def solution():
+    max_num = max(nums)
+    is_prime = sieve(max_num)
+
+    results = []
+    for num in nums:
+        a, b = find_goldbach_pair(num, is_prime)
+        results.append(f"{a} {b}")
+
+    print("\n".join(results))
+
+
+solution()

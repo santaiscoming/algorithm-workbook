4b5e30d3ea03384dac5d9c39295ad4bbf7870f39
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-01-07 21:44:27 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,46 +1,27 @@
 import sys
-import heapq
 
 sys.setrecursionlimit(10**8)
-# sys.stdin = open("./input.txt", "r")  # 제거
 input = sys.stdin.readline
 
-
-N = int(input())
-
-
-# def fibo(n):
-#     dp = [0] * (n + 1)
-#     dp[0] = 0
-#     dp[1] = 1
-
-#     # f(n) = f(n-1) + f(n - 2)
-#     for i in range(2, n + 1):
-#         dp[i] = dp[i - 1] + dp[i - 2]
-
-#     return dp[n]
+n = int(input())
 
 
-dp = [0] * (N + 1)
-dp[1] = 1
+def solution(n: int):
+    memo = {}
+
+    def fibo(n):
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+
+        if memo.get(n):
+            return memo.get(n)
+        else:
+            memo[n] = fibo(n - 1) + fibo(n - 2)
+            return memo[n]
+
+    print(fibo(n))
 
 
-def fibo_recur(n):
-    if n == 1 or n == 0:
-        return dp[n]
-
-    if dp[n] != 0 and n != 0:
-        return dp[n]
-
-    dp[n] = fibo_recur(n - 1) + fibo_recur(n - 2)
-
-    return dp[n]
-
-
-def solution(n):
-    result = fibo_recur(n)
-
-    print(result)
-
-
-solution(N)
+solution(n)

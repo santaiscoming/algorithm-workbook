8c14fc212c3a4baaeff7858654d2121ffc28fdc4
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-01-09 05:02:47 +0900

    [Gold V] Title: 동전, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,35 +1,37 @@
 import sys
 
 sys.setrecursionlimit(10**8)
-# sys.stdin = open("./input.txt", "r")  # 제거
 input = sys.stdin.readline
+
+global T
+global cases
+
 
 T = int(input())
 cases = [
     [int(input()), list(map(int, input().split())), int(input())] for _ in range(T)
 ]
 
+# idx : price
+# memo[idx] : 경우의 수
 
-def solution(cases):
+# ex)
+# coins = [1, 2], price = 5
+# memo[idx] = memo[idx - coin]
+# 5원 를 만들기 위한 경우의 수 = 3원의 경우의 수 + 2원 1개사용
+
+
+def solution():
     for case in cases:
-        N, coins, target_money = case
+        _, coins, price = case
 
-        table = [[0] * (target_money + 1) for y in range(N + 1)]
+        memo = [0] * (price + 1)
+        memo[0] = 1
+        for coin in coins:
+            for i_p in range(coin, price + 1):
+                memo[i_p] += memo[i_p - coin]
 
-        for y in range(1, N + 1):
-            # 4원을 예시로 들었을때 0원을 만드는 경우는 4원을 쓰지않는 경우가 있으니까
-            table[y][0] = 1
-            cur_coin = coins[y - 1]
-
-            for x in range(1, target_money + 1):
-                # case 1 :본인보다 용량이 작은경우
-                if cur_coin > x:
-                    table[y][x] = table[y - 1][x]
-                else:
-                    # case 2 : 자기자신을 쓰는 경우의 수 + 자기자신을 전혀 안쓰는 경우의 수
-                    table[y][x] = table[y][x - cur_coin] + table[y - 1][x]
-
-        print(table[-1][-1])
+        print(memo[i_p])
 
 
-solution(cases)
+solution()

8b58e8340538df55e25e030233b6a6de88b6cb64
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-02-08 22:39:49 +0900

    [Gold IV] Title: N-Queen, Time: 20008 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,46 +1,39 @@
 import sys
 
-
 sys.setrecursionlimit(10**8)
 # sys.stdin = open("./input.txt", "r")  # 제거
 input = sys.stdin.readline
 
-N = int(input())
 
-visited_col = [False] * (N * 2)
-visited_우상향 = [False] * (N * 2)
-visited_우하향 = [False] * (N * 2)
+N = int(input())
+# k -> row, board[k] -> col (놓을 수 있는 위치)
+board = [False] * N
+board_right_up = [False] * (N * 2)
+board_right_down = [False] * (N * 2)
 count = 0
 
 
-# n : 퀸을 놓은 곳 (0부터시작)
-# current_col : x축 and 행
-def queen(curr_row):
+def queen(row):
     global count
 
-    # 현재 말을 놓으려는 곳이 N이 되면 0부터 시작해서 모두 놓은것
-    if curr_row == N:
+    if row >= N:
         count += 1
         return
 
-    for y in range(0, N):
-        # **continue** 조건
-        # if (열에 퀸이 있거나 visited[y축])
-        # if (우상향 대각이 있거나 visited[x + y]) -> 좌표가 같다
-        # if (우하향 대각이 있으면 (y - x - n - 1 => 1차원 배열이기에 대각선 모두 같은 값을 보장해야함으로))
+    for col in range(0, N):
         if (
-            visited_col[y]
-            or visited_우상향[curr_row + y]
-            or visited_우하향[(curr_row - y + N - 1)]
+            board[col]
+            or board_right_up[row + col]
+            or board_right_down[(row - col + N - 1)]
         ):
             continue
-        visited_col[y] = True
-        visited_우상향[curr_row + y] = True
-        visited_우하향[curr_row - y + N - 1] = True
-        queen(curr_row + 1)
-        visited_col[y] = False
-        visited_우상향[curr_row + y] = False
-        visited_우하향[curr_row - y + N - 1] = False
+        board[col] = True
+        board_right_up[row + col] = True
+        board_right_down[row - col + N - 1] = True
+        queen(row + 1)
+        board[col] = False
+        board_right_up[row + col] = False
+        board_right_down[row - col + N - 1] = False
 
 
 queen(0)

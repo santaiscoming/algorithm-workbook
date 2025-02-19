412e0176d958db8f8f516646d009721eede18f62
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-02-19 23:19:05 +0900

    [Gold IV] Title: 미로만들기, Time: 142 ms, Memory: 42940 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,6 +1,8 @@
 import sys
 from collections import deque
+import heapq
 import math
+from typing import Tuple, TypeAlias
 
 # sys.stdin = open("./input.txt", "r")  # 제거
 input = sys.stdin.readline
@@ -12,8 +14,36 @@ global black, white
 black = 0
 white = 1
 
+Tx: TypeAlias = int
+Ty: TypeAlias = int
 
-def solution(sx, sy):
+
+def dijkstra_solution(sx: int, sy: int) -> None:
+    cost = [[math.inf] * n for _ in range(n)]
+    cost[sy][sx] = 0
+
+    q: Tuple[int, Tuple[Tx, Ty]] = []
+    heapq.heappush(q, (cost[sy][sx], (sx, sy)))
+
+    while q:
+        prev_cost, (x, y) = heapq.heappop(q)
+
+        for dy, dx in dir:
+            ny, nx = dy + y, dx + x
+            if (0 <= nx < n) and (0 <= ny < n):
+                additional_cost = 1 if mat[ny][nx] == black else 0
+
+                if cost[ny][nx] > prev_cost + additional_cost:
+                    cost[ny][nx] = prev_cost + additional_cost
+                    heapq.heappush(q, (cost[ny][nx], (nx, ny)))
+
+    print(cost[-1][-1])
+
+
+dijkstra_solution(0, 0)
+
+
+def bfs_solution(sx, sy):
     cost = [[math.inf] * n for _ in range(n)]
     cost[sy][sx] = 0
 
@@ -35,4 +65,4 @@ def solution(sx, sy):
     print(cost[-1][-1])
 
 
-solution(0, 0)
+# bfs_solution(0, 0)

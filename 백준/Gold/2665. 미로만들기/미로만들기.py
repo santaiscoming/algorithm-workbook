bca76fe09f703f9646c3f81aee509220fa67a24c
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-02-19 22:55:09 +0900

    [Gold IV] Title: 미로만들기, Time: 68 ms, Memory: 36684 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+import math
+
+# sys.stdin = open("./input.txt", "r")  # 제거
+input = sys.stdin.readline
+
+n = int(input())
+mat = [list(map(int, list(input().rstrip()))) for _ in range(n)]
+dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+global black, white
+black = 0
+white = 1
+
+
+def solution(sx, sy):
+    cost = [[math.inf] * n for _ in range(n)]
+    cost[sy][sx] = 0
+
+    q = deque()
+    q.append((sx, sy))
+
+    while q:
+        x, y = q.popleft()
+
+        for dy, dx in dir:
+            ny, nx = dy + y, dx + x
+
+            if (0 <= ny < n) and (0 <= nx < n):
+                additional_cost = 1 if mat[ny][nx] == black else 0
+                if cost[ny][nx] > cost[y][x] + additional_cost:
+                    cost[ny][nx] = cost[y][x] + additional_cost
+                    if additional_cost == 0:
+                        q.appendleft((nx, ny))
+                    else:
+                        q.append((nx, ny))
+
+    print(cost[-1][-1])
+
+
+solution(0, 0)

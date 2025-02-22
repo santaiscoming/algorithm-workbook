6bd655c4d8baeffb470eaafa79062b3623963815
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-02-23 05:09:51 +0900

    [Gold IV] Title: 사냥꾼, Time: 552 ms, Memory: 56052 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,30 +1,49 @@
 import sys
-from typing import Tuple
+from typing import Tuple, List
+import math
 
 # sys.stdin = open("./input.txt", "r")
 input = sys.stdin.readline
 
 # (M: 사대 수), (N: 동물 수), (L: 사정거리)
 M, N, L = map(int, input().split())
-shooter_pos = list(map(int, input().split()))
-animal_pos = [list(map(int, input().split())) for _ in range(N)]
+shooters: List[int] = list(map(int, input().split()))
+animals: List[Tuple[int, int]] = [tuple(map(int, input().split())) for _ in range(N)]
+
+
+def get_distance(sx: int, aPos: Tuple[int, int]) -> int:
+    (ax, ay) = aPos
+    return abs(sx - ax) + ay
+
+
+def binary_search(arr: List[int], target, left, right):
+    if left >= right:
+        return left
+
+    mid = (left + right) // 2
+
+    if arr[mid] > target:
+        return binary_search(arr, target, left, mid)
+    else:
+        return binary_search(arr, target, mid + 1, right)
 
 
 def solution():
+    shooters.sort()
+
     cnt = 0
-    dead = []
 
-    def get_distance(sx: int, aPos: Tuple[int, int]):
-        (ax, ay) = aPos
-        return abs(sx - ax) + ay
+    for aPos in animals:
+        ax, ay = aPos
+        idx = binary_search(shooters, ax, 0, len(shooters) - 1)
 
-    for s_x in shooter_pos:
-        for aPos in animal_pos:
-            if aPos in dead:
-                continue
-            if L >= get_distance(s_x, aPos):
-                cnt += 1
-                dead.append(aPos)
+        dist = math.inf
+        if idx < len(shooters):
+            dist = min(dist, get_distance(shooters[idx], aPos))
+        if idx - 1 >= 0:
+            dist = min(dist, get_distance(shooters[idx - 1], aPos))
+        if dist <= L:
+            cnt += 1
 
     print(cnt)
 

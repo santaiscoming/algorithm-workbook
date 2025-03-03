014ab5b4e46f73f55f61060e7ca5ec3f3293fe92
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-03-03 23:28:06 +0900

    [Platinum V] Title: 찾기, Time: 592 ms, Memory: 84836 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.py
+++ 백준/Platinum/1786. 찾기/찾기.py
@@ -8,10 +8,45 @@ T, P = list(input().rstrip()), list(input().rstrip())
 
 def solution():
 
-    result = kmp_search(T, P)
+    result = kmp_match(T, P)
     print(len(result))
     for num in result:
         print(num + 1)
+
+
+def kmp_match(txt, pattern):
+    pt = 1
+    pp = 0
+    skip = [0] * (len(pattern) + 1)
+    result = []
+
+    skip[pt] = 0
+    while pt != len(pattern):
+        if pattern[pt] == pattern[pp]:
+            pt += 1
+            pp += 1
+            skip[pt] = pp
+        elif pp == 0:
+            pt += 1
+            skip[pt] = 0
+        else:
+            pp = skip[pp]
+
+    pt, pp = 0, 0
+    while pt != len(txt) and pp != len(pattern):
+        if txt[pt] == pattern[pp]:
+            pt += 1
+            pp += 1
+        elif pp == 0:
+            pt += 1
+        else:
+            pp = skip[pp]
+
+        if pp == len(pattern):
+            result.append(pt - pp)
+            pp = skip[pp]
+
+    return result
 
 
 def compute_lps(pattern):
@@ -45,27 +80,28 @@ def kmp_search(text, pattern):
     텍스트에서 패턴을 KMP 방식으로 검색하여, 패턴이 시작되는 모든 인덱스를 리스트로 반환합니다.
     """
     lps = compute_lps(pattern)
+    print(lps)
     positions = []  # 패턴이 일치하는 시작 인덱스를 저장할 리스트
-    i = 0  # text의 인덱스
-    j = 0  # pattern의 인덱스
+    pt = 0  # text의 인덱스
+    pp = 0  # pattern의 인덱스
 
-    while i < len(text):
-        if text[i] == pattern[j]:
-            i += 1
-            j += 1
+    while pt < len(text):
+        if text[pt] == pattern[pp]:
+            pt += 1
+            pp += 1
 
         # 만약 j가 패턴의 끝에 도달하면, 패턴이 발견된 것입니다.
-        if j == len(pattern):
-            positions.append(i - j)
+        if pp == len(pattern):
+            positions.append(pt - pp)
             # 다음 일치를 위해 j를 lps[j-1]로 이동
-            j = lps[j - 1]
-        elif i < len(text) and text[i] != pattern[j]:
+            pp = lps[pp - 1]
+        elif pt < len(text) and text[pt] != pattern[pp]:
             # 불일치가 발생한 경우
-            if j != 0:
+            if pp != 0:
                 # 이전에 일치했던 길이를 활용하여 패턴 포인터를 이동
-                j = lps[j - 1]
+                pp = lps[pp - 1]
             else:
-                i += 1
+                pt += 1
     return positions
 
 

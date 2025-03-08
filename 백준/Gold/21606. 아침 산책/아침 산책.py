e4ed86ec65a3fa3a66384548a52a233fcf5115a6
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-03-08 18:49:35 +0900

    [Gold III] Title: 아침 산책, Time: 1532 ms, Memory: 107820 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -5,50 +5,36 @@ from itertools import combinations, permutations
 input = sys.stdin.readline
 
 N = int(input())
-strArr = list(input().rstrip())
+str = "2" + input().rstrip()
 edges = [tuple(map(int, input().split())) for _ in range(N - 1)]
 
 
 def solution():
     cnt = 0
-    graph = [[] for _ in range(N + 1)]
     visited = [False] * (N + 1)
+    graph = [[] for _ in range(N + 1)]
     for u, v in edges:
-        graph[u].append((v, bool(int(strArr[v - 1]))))
-        graph[v].append((u, bool(int(strArr[u - 1]))))
+        graph[u].append((v, bool(int(str[v]))))
+        graph[v].append((u, bool(int(str[u]))))
 
-    combi = list(permutations([x + 1 for x in range(N)], 2))
-
-    def cmp(edge):
-        u, v = edge
-
-        if strArr[u - 1] == "1" and strArr[v - 1] == "1":
-            return True
-
-        return False
-
-    routes = list(filter(cmp, combi))
-
-    def dfs(start, end):
+    def dfs(start):
         nonlocal cnt
 
         for edge in graph[start]:
             v, isInside = edge
-            if end == v and strArr[v - 1] == "1":
-                cnt += 1
-                continue
-            if not visited[v] and not isInside:
 
+            if isInside and not visited[v]:
+                cnt += 1
+            elif not visited[v] and not isInside:
                 visited[v] = True
-                dfs(v, end)
+                dfs(v)
                 visited[v] = False
 
-    for route in routes:
-        s, v = route
-
-        visited[s] = True
-        dfs(s, v)
-        visited[s] = False
+    for s in range(1, N + 1):
+        if str[s] == "1":
+            visited[s] = True
+            dfs(s)
+            visited[s] = False
 
     print(cnt)
 

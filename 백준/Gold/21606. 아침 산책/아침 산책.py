90563bf1d1dee2d4f875e23bfd89fc65f9b6d163
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-03-08 19:53:09 +0900

    [Gold III] Title: 아침 산책, Time: 1176 ms, Memory: 151788 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,42 +1,46 @@
 import sys
-from itertools import combinations, permutations
+
+sys.setrecursionlimit(10**6)
 
 # sys.stdin = open("./input.txt", "r")  # 제거
 input = sys.stdin.readline
 
 N = int(input())
-str = "2" + input().rstrip()
+inside = "2" + input().rstrip()
 edges = [tuple(map(int, input().split())) for _ in range(N - 1)]
 
 
 def solution():
-    cnt = 0
-    visited = [False] * (N + 1)
+    result = 0
     graph = [[] for _ in range(N + 1)]
     for u, v in edges:
-        graph[u].append((v, bool(int(str[v]))))
-        graph[v].append((u, bool(int(str[u]))))
+        graph[u].append((v, bool(int(inside[v]))))
+        graph[v].append((u, bool(int(inside[u]))))
+        if inside[u] == "1" and inside[v] == "1":
+            result += 2
+    visited = [False] * (N + 1)
 
     def dfs(start):
-        nonlocal cnt
+        inNodeCnt = 0
 
-        for edge in graph[start]:
-            v, isInside = edge
+        for neighbor in graph[start]:
+            v, isInside = neighbor
 
-            if isInside and not visited[v]:
-                cnt += 1
+            if isInside:
+                inNodeCnt += 1
             elif not visited[v] and not isInside:
                 visited[v] = True
-                dfs(v)
-                visited[v] = False
+                inNodeCnt += dfs(v)
+
+        return inNodeCnt
 
     for s in range(1, N + 1):
-        if str[s] == "1":
+        if inside[s] == "0" and not visited[s]:
             visited[s] = True
-            dfs(s)
-            visited[s] = False
+            nodeCnt = dfs(s)
+            result += nodeCnt * (nodeCnt - 1)
 
-    print(cnt)
+    print(result)
 
 
 solution()

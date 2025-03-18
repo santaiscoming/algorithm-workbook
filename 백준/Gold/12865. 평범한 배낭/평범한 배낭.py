64f323163ded45d783e2aab198eb3fe648b53bc6
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-03-18 17:07:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 7684 ms, Memory: 165176 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,30 +1,105 @@
 import sys
+import math
 
-sys.setrecursionlimit(10**8)
-# sys.stdin = open("./input.txt", "r")  # 제거
+# sys.stdin = open("./input.txt", "r")  # 제출 전 제거
 input = sys.stdin.readline
-# N : 물품개수, K : 무게
-N, K = map(int, input().split())
-# (W, V) : 무게, 가치
-items = [list(map(int, input().split())) for _ in range(N)]
+
+n, k = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(n)]
 
 
-def solution(items):
-    table = [[0] * (K + 1) for _ in range(N + 1)]
+# i 값이 0에서 시작하는것과 n-1 부터 시작하는것의 차이가 무엇을 의미하는지 생각하기
+def __dpSolution():
+    memo = [[-1] * (k + 1) for _ in range(n)]
+    s = [size for size, _ in items]
+    p = [weight for _, weight in items]
 
-    for y in range(1, N + 1):
-        cur_w, cur_v = items[y - 1]
+    def dfs(i, size):
+        print(i)
+        [print(r) for r in memo]
+        print("")
 
-        for bag_w in range(1, K + 1):
-            prev_item_table_acc = table[y - 1][bag_w]
+        if i >= n:
+            return 0
 
-            if bag_w >= cur_w:
-                max_cur_item_val = table[y - 1][bag_w - cur_w] + cur_v
-                table[y][bag_w] = max(max_cur_item_val, prev_item_table_acc)
+        if memo[i][size] != -1:
+            return memo[i][size]
+
+        if size - s[i] >= 0:
+            memo[i][size] = max(dfs(i + 1, size - s[i]) + p[i], dfs(i + 1, size))
+
+        memo[i][size] = max(memo[i][size], dfs(i + 1, size))
+
+        return memo[i][size]
+
+    print(dfs(0, k))
+
+
+def dpSolution():
+    memo = [[-1] * (k + 1) for _ in range(n)]
+    s = [size for size, _ in items]
+    p = [weight for _, weight in items]
+
+    def dfs(i, size):
+
+        if i < 0:
+            return 0
+
+        if memo[i][size] != -1:
+            return memo[i][size]
+
+        if size - s[i] >= 0:
+            memo[i][size] = max(dfs(i - 1, size - s[i]) + p[i], dfs(i - 1, size))
+
+        memo[i][size] = max(memo[i][size], dfs(i - 1, size))
+
+        return memo[i][size]
+
+    dfs(n - 1, k)
+    print(memo[-1][-1])
+
+
+dpSolution()
+
+
+def fractionalSolution():
+    items.sort(key=lambda x: (x[1] / x[0]), reverse=True)
+    mp = 0
+
+    def fractional(i, size):
+        result = 0
+        for j in range(i, n):
+            w, p = items[j]
+
+            if size >= w:
+                result += p
+                size -= w
             else:
-                table[y][bag_w] = prev_item_table_acc
+                result += (p / w) * size
+                break
 
-    print(table[-1][-1])
+        return result
+
+    def knapsack(i, size, acc):
+        nonlocal mp
+
+        if acc + fractional(i, size) < mp:
+            return
+
+        if i >= n or size <= 0:
+            mp = max(mp, acc)
+            return
+
+        w, p = items[i]
+
+        if size >= w:
+            knapsack(i + 1, size - w, acc + p)
+
+        knapsack(i + 1, size, acc)
+
+    knapsack(0, k, 0)
+
+    print(mp)
 
 
-solution(items)
+# fractionalSolution()

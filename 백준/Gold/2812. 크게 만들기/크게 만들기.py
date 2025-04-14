018294cf236138f1898ef4293bda0cf7fa1c281b
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-04-14 10:25:10 +0900

    [Gold III] Title: 크게 만들기, Time: 288 ms, Memory: 54180 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,35 @@
+import sys
+
+# sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+num = list(input().strip())
+
+
+def solution():
+    maximum_stack_count = n - k
+    stack = []
+    for i, v in enumerate(num):
+        v = int(v)
+        appendable_count = maximum_stack_count - len(stack)
+        rest = n - i
+
+        if not stack:
+            stack.append(v)
+            continue
+
+        if rest < appendable_count:
+            stack.append(v)
+            continue
+
+        while stack and v > stack[-1] and maximum_stack_count - len(stack) < rest:
+            stack.pop()
+
+        if maximum_stack_count - len(stack) > 0:
+            stack.append(v)
+
+    print("".join(list(map(str, stack))))
+
+
+solution()

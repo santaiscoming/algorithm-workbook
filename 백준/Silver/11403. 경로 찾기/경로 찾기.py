428f97d9c7791ba3c518cae10588c231f9707d86
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-04-16 22:05:14 +0900

    [Silver I] Title: 경로 찾기, Time: 92 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,44 +1,29 @@
 import sys
 from collections import deque
 
-# sys.stdin = open("./input.txt", "r")  # 제거
 input = sys.stdin.readline
 
 n = int(input())
-adj_mat = [list(map(int, input().split())) for _ in range(n)]
-
+graph = [list(map(int, input().split())) for _ in range(n)]
+result = [[0] * n for _ in range(n)]
 
 def solution():
-    result = [[0] * n for _ in range(n)]
+    def bfs(start):
+        visited = [False] * n
+        queue = deque([start])
 
-    def bfs(s, _from, depth):
-        visited = [[False] * n for _ in range(n)]
-        q = deque()
-        q.append((s, _from, depth))
+        while queue:
+            now = queue.popleft()
+            for i in range(n):
+                if graph[now][i] == 1 and not visited[i]:
+                    visited[i] = True
+                    result[start][i] = 1
+                    queue.append(i)
 
-        while q:
-            u, f, d = q.popleft()
+    for i in range(n):
+        bfs(i)
 
-            for v in range(n):
-                if u == v:
-                    continue
-
-                if f == v and d > 0 and adj_mat[u][v] == 1:
-                    visited[f][f] = True
-                    result[f][f] = 1
-                    continue
-
-                if not visited[u][v] and adj_mat[u][v] == 1:
-                    visited[u][v] = True
-                    result[u][v] = 1
-                    result[_from][v] = 1
-                    q.append((v, f, d + 1))
-
-    for s in range(n):
-        bfs(s, s, 0)
-
-    for v in result:
-        print(*v)
-
-
+    for row in result:
+        print(*row)
+        
 solution()

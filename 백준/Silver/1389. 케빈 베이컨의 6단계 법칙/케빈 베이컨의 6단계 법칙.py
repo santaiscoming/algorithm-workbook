a831cf8004b0e52f0f0a1b821653357fc711aecc
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-04-21 21:43:13 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 36388 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 import math
 
 # sys.stdin = open("input.txt", "r")
@@ -6,6 +7,37 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 edges = [list(map(int, input().split())) for _ in range(m)]
+
+
+def bfs_solution():
+    graph = [[] for _ in range(n)]
+    for s, v in edges:
+        graph[s - 1].append(v - 1)
+        graph[v - 1].append(s - 1)
+
+    def bfs(start):
+        result = [0] * n
+        visited = [False] * n
+        visited[start] = True
+
+        q = deque([start])
+
+        while q:
+            s = q.popleft()
+
+            for v in graph[s]:
+                if not visited[v]:
+                    result[v] = result[s] + 1
+                    visited[v] = True
+                    q.append(v)
+
+        return sum(v for v in result)
+
+    kevin_rate = [(i + 1, bfs(i)) for i in range(n)]
+    print(min(kevin_rate, key=lambda x: (x[1], x[0]))[0])
+
+
+bfs_solution()
 
 
 def solution():
@@ -32,4 +64,4 @@ def solution():
     print(kevin_rate.index(min(kevin_rate)) + 1)
 
 
-solution()
+# solution()

987da1a7e76a48f0f8389bb52fb69e88640ef512
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-05-16 18:08:09 +0900

    [Silver IV] Title: 문자열 집합, Time: 4124 ms, Memory: 43512 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -9,7 +9,40 @@ searchStrings = [input().strip() for _ in range(m)]
 
 
 def solution():
-    print(sum(1 for s in searchStrings if s in set(stringSet)))
+    class Node:
+        def __init__(self) -> None:
+            self.child = {}
+            self.isEnd = False
+
+        def setIsEnd(self, bool):
+            self.isEnd = bool
+
+    class Trie:
+        def __init__(self) -> None:
+            self.root = Node()
+
+        def add(self, word):
+            node = self.root
+
+            for ch in word:
+                node = node.child.setdefault(ch, Node())
+
+            node.setIsEnd(True)
+
+        def search(self, word) -> bool:
+            node = self.root
+
+            for ch in word:
+                if ch not in node.child:
+                    return False
+
+                node = node.child[ch]
+
+            return node.isEnd
+
+    trie = Trie()
+    [trie.add(s) for s in stringSet]
+    print(sum(trie.search(s) for s in searchStrings))
 
 
 solution()

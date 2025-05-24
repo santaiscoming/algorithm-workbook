07ed0636ade15de70eaf879cc6785bb7ffefba46
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-05-24 22:16:18 +0900

    [Gold V] Title: 합분해, Time: 40 ms, Memory: 33564 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -8,6 +8,26 @@ n, k = map(int, input().split())
 
 def solution():
     MOD = 1_000_000_000
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+    for j in range(k + 1):
+        dp[0][j] = 1
+
+    for i in range(n + 1):
+        dp[i][1] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, k + 1):
+            dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD
+
+    print(dp[n][k])
+
+
+solution()
+
+
+def solution2():
+    MOD = 1_000_000_000
     dp = [[0] * (n + 1) for _ in range(k + 1)]
 
     for j in range(n + 1):
@@ -21,4 +41,4 @@ def solution():
     print(dp[k][n])
 
 
-solution()
+# solution2()

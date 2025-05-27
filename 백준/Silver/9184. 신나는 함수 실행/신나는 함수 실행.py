c29e83c934c966538938f342753c0e4a39ecc8a3
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-05-27 20:18:54 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 112 ms, Memory: 43268 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,49 @@
+import sys
+from itertools import takewhile
+
+
+# sys.stdin = open("input.txt", "r")  # ❌
+input = sys.stdin.readline
+
+
+inputs = list(
+    takewhile(
+        lambda arr: not all(i == -1 for i in arr),
+        iter(lambda: list(map(int, input().split())), None),
+    )
+)
+
+
+def solution():
+    MAX_RANGE = 103
+    dp = [[[0] * MAX_RANGE for _ in range(MAX_RANGE)] for _ in range(MAX_RANGE)]
+
+    def w(a, b, c) -> str:
+        if dp[a][b][c]:
+            return dp[a][b][c]
+
+        result = 0
+
+        if any(num <= 0 for num in [a, b, c]):
+            result = 1
+        elif any(num > 20 for num in [a, b, c]):
+            result = w(20, 20, 20)
+        elif a < b < c:
+            result = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        else:
+            result = (
+                w(a - 1, b, c)
+                + w(a - 1, b - 1, c)
+                + w(a - 1, b, c - 1)
+                - w(a - 1, b - 1, c - 1)
+            )
+
+        dp[a][b][c] = result
+
+        return result
+
+    for a, b, c in inputs:
+        print(f"w({a}, {b}, {c}) = {w(a, b,c)}")
+
+
+solution()

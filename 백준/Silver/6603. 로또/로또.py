021be40d6a0efc5939560cd93a67ef27b22a8fed
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-02 17:13:08 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -5,24 +5,24 @@ input = sys.stdin.readline
 
 tcs = [list(map(int, line.split())) for line in iter(lambda: input().rstrip(), "0")]
 
+# 조합일땐 visited 필요
+# 순열일땐 불필요 start가 이미 제외
+
 
 def solution():
-    def dfs(n, nums, lottery, visited, depth, j):
+    def dfs(nums, lottery, depth, start):
         if depth == 6:
             print(*lottery)
             return
 
-        for i in range(j, n):
-            if not visited[i]:
-                lottery.append(nums[i])
-                visited[i] = True
-                dfs(n, nums, lottery, visited, depth + 1, i + 1)
-                visited[i] = False
-                lottery.pop()
+        for i in range(start, len(nums)):
+            lottery.append(nums[i])
+            dfs(nums, lottery, depth + 1, i + 1)
+            lottery.pop()
 
     for i, v in enumerate(tcs):
-        n, *rest = v
-        dfs(n, rest, [], [False] * n, 0, 0)
+        _, *rest = v
+        dfs(rest, [], 0, 0)
         print("") if i != len(tcs) - 1 else None
 
 

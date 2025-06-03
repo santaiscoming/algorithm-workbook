fc9f93fcc4259a7324ff823a83867b210829ecfc
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-03 17:26:05 +0900

    [Silver II] Title: 차이를 최대로, Time: 108 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,49 +1,37 @@
 import sys
 
-
-# sys.stdin = open("input.txt", "r")
+# sys.stdin = open("input.txt", "r")  # ❌
 input = sys.stdin.readline
 
-
 n = int(input())
-numss = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 
 
 def solution():
-    maximum = 0
-    visited = [False] * n
+    def sum(arr):
+        result = 0
 
-    def calc(nums):
-        acc = 0
+        for i in range(1, len(arr)):
+            result += abs(arr[i - 1] - arr[i])
 
-        for i in range(1, len(nums)):
-            acc += abs(nums[i - 1] - nums[i])
+        return result
 
-        return acc
+    result = 0
 
-    def recur(nums):
-        nonlocal maximum
+    def dfs(arr, visited):
+        nonlocal result
 
-        if len(nums) == n:
-            cur_value = calc(nums)
+        if len(arr) == n:
+            result = max(sum(arr), result)
+        else:
+            for i in range(n):
+                if not visited[i]:
+                    visited[i] = True
+                    dfs(arr + [nums[i]], visited)
+                    visited[i] = False
 
-            if cur_value > maximum:
-                maximum = cur_value
-
-            return
-
-        for i, num in enumerate(numss):
-            if not visited[i]:
-                visited[i] = True
-                recur([*nums, num])
-                visited[i] = False
-
-    for i, num in enumerate(numss):
-        visited[i] = True
-        recur([num])
-        visited[i] = False
-
-    print(maximum)
+    dfs([], [False] * n)
+    print(result)
 
 
 solution()

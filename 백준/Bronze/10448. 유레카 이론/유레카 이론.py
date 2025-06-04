99a6b8354339c29e18b10c6f5bf00c67551b2fe2
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-04 21:50:13 +0900

    [Bronze I] Title: 유레카 이론, Time: 288 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.py
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.py
@@ -8,25 +8,23 @@ nums = [int(input()) for _ in range(n)]
 
 
 def solution():
-    max_삼각수 = next(i for i in range(10000) if (i * (i + 1)) / 2 >= 1000)
-    삼각수 = [int((j * (j + 1)) / 2) for j in range(1, max_삼각수 + 1)]
+    max_n = next(i for i in range(10000) if (i * (i + 1)) / 2 >= 1000)
+    삼각수s = [int((j * (j + 1)) / 2) for j in range(1, max_n + 1)]
 
-    def dfs(depth, s, isPossible):
-        if isPossible[0]:
-            return
-
+    def dfs(depth, s):
         if depth == 3:
             if s == 0:
-                isPossible[0] = True
-            return
+                return True
+        else:
+            for i in 삼각수s:
+                is삼각수 = dfs(depth + 1, s - i)
+                if is삼각수:
+                    return True
 
-        for i in 삼각수:
-            dfs(depth + 1, s - i, isPossible)
+        return False
 
     for num in nums:
-        isPossible = [False]
-        dfs(0, num, isPossible)
-        if isPossible[0]:
+        if dfs(0, num):
             print(1)
         else:
             print(0)

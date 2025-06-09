c9635c8dd4b104ef8feefaa8f9d931e756a99848
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-09 21:10:19 +0900

    [Silver I] Title: 스타트와 링크, Time: 1968 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -6,6 +6,44 @@ input = sys.stdin.readline
 
 n = int(input())
 mat = [list(map(int, input().split())) for _ in range(n)]
+INF = float("inf")
+
+
+def recur_solution():
+    new_mat = [[mat[i][j] + mat[j][i] for i in range(n)] for j in range(n)]
+
+    def calc(picked):
+        a_team = picked
+        b_team = [i for i in range(n) if i not in a_team]
+
+        a_score = 0
+        b_score = 0
+
+        for i in range(n // 2):
+            for j in range(i, n // 2):
+                a_score += new_mat[a_team[i]][a_team[j]]
+                b_score += new_mat[b_team[i]][b_team[j]]
+
+        return abs(a_score - b_score)
+
+    def dfs(picked, start_idx, visited):
+        nonlocal result
+
+        if len(picked) == n // 2:
+            result = min(result, calc(picked))
+
+        for i in range(start_idx, n):
+            if not visited[i]:
+                visited[i] = True
+                dfs(picked + [i], i, visited)
+                visited[i] = False
+
+    result = INF
+    dfs([], 0, [False] * n)
+    print(result)
+
+
+recur_solution()
 
 
 def combi_solution():
@@ -14,13 +52,15 @@ def combi_solution():
         b_score = 0
 
         for i in range(n // 2):
-            for j in range(n // 2):
+            for j in range(i, n // 2):
                 a_score += mat[a_team[i]][a_team[j]]
+                a_score += mat[a_team[j]][a_team[i]]
                 b_score += mat[b_team[i]][b_team[j]]
+                b_score += mat[b_team[j]][b_team[i]]
 
         return abs(a_score - b_score)
 
-    result = float("inf")
+    result = INF
 
     for combi in combinations(range(n), n // 2):
         a_team = list(combi)
@@ -31,7 +71,7 @@ def combi_solution():
     print(result)
 
 
-combi_solution()
+# combi_solution()
 
 
 def solution():
@@ -48,32 +88,29 @@ def solution():
 
         return abs(a_score - b_score)
 
-    def dfs(a_team, b_team, visited):
-        print(a_team, b_team)
+    def dfs(a_team, b_team, visited, start):
         nonlocal result
 
         if len(a_team) + len(b_team) == n:
             result = min(result, calc(a_team, b_team))
 
         if len(a_team) < n // 2:
-            for i in range(n):
+            for i in range(start, n):
                 if not visited[i]:
                     visited[i] = True
-                    dfs(a_team + [i], b_team, visited)
+                    dfs(a_team + [i], b_team, visited, start + 1)
                     visited[i] = False
 
         if len(b_team) < n // 2:
-            for i in range(n):
+            for i in range(start, n):
                 if not visited[i]:
                     visited[i] = True
-                    dfs(a_team, b_team + [i], visited)
+                    dfs(a_team, b_team + [i], visited, start + 1)
                     visited[i] = False
 
-    INF = float("inf")
     result = INF
-    dfs([], [], [False] * n)
+    dfs([], [], [False] * n, 0)
     print(result)
-    print(2**20)
 
 
 # solution()

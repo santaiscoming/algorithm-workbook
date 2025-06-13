da2f3cf17f48b48841fe921fd8d6e432b40fe541
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-13 21:51:32 +0900

    [Silver III] Title: N과 M (3), Time: 1784 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -6,6 +6,24 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 
+def solution2():
+    stack = [[]]
+
+    while stack:
+        nums = stack.pop()
+
+        if len(nums) == m:
+            print(*nums)
+            continue
+
+        for i in range(n, 0, -1):
+            next = nums + [i]
+            stack.append(next)
+
+
+solution2()
+
+
 def solution():
     def dfs(nums, depth):
         if depth == m:
@@ -18,4 +36,4 @@ def solution():
     dfs([], 0)
 
 
-solution()
+# solution()

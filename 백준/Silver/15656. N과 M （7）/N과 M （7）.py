309d018f32aecd76dab25efcb903b7113e2587bc
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-14 20:41:08 +0900

    [Silver III] Title: N과 M (7), Time: 2024 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -7,6 +7,25 @@ n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
 
+def solution2():
+    nums.sort(reverse=True)
+
+    stack = [[]]
+
+    while stack:
+        curr = stack.pop()
+
+        if len(curr) == m:
+            print(*curr)
+            continue
+
+        for num in nums:
+            stack.append([*curr, num])
+
+
+solution2()
+
+
 def solution():
     nums.sort()
 
@@ -21,4 +40,4 @@ def solution():
     dfs([])
 
 
-solution()
+# solution()

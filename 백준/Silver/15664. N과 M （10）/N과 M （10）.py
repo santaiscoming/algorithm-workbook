8ae39880ae2bc8a20efaf5caecce4df8536d99b3
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-14 21:44:25 +0900

    [Silver II] Title: N과 M (10), Time: 60 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,5 +1,6 @@
 import sys
 from collections import Counter
+from itertools import combinations
 
 # sys.stdin = open("input.txt", "r")  # ❌
 input = sys.stdin.readline
@@ -8,24 +9,8 @@ n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
 
-def solution3():
-    counter = Counter(nums)
-    uniqueNums = sorted(counter.keys())
-
-    def dfs(picked, start):
-        if len(picked) == m:
-            print(*picked)
-            return
-
-        for i in range(start, len(uniqueNums)):
-
-            num = uniqueNums[i]
-            if counter[num] > 0:
-                counter[num] -= 1
-                dfs([*picked, num], i)
-                counter[num] += 1
-
-    dfs([], 0)
+def solution():
+    [print(*combi) for combi in sorted(set(combinations(sorted(nums), m)))]
 
 
-solution3()
+solution()

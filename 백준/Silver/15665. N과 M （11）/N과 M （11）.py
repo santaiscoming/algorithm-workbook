27c19b56e04092cd7c9bebfe4d5bb05055116f32
백현영 <116708517+santaiscoming@users.noreply.github.com>
2025-06-14 21:55:24 +0900

    [Silver II] Title: N과 M (11), Time: 708 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,6 +1,7 @@
 import sys
 from itertools import product
 
+
 # sys.stdin = open("input.txt", "r")  # ❌
 input = sys.stdin.readline
 
@@ -8,8 +9,29 @@ n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
 
+def solution2():
+    nums.sort()
+
+    def dfs(picked):
+        if len(picked) == m:
+            print(*picked)
+            return
+
+        prev = -1
+        for i in range(n):
+            if nums[i] != prev:
+                prev = nums[i]
+                dfs([*picked, nums[i]])
+
+    dfs([])
+
+
+solution2()
+
+
 def solution():
+
     [print(*combi) for combi in sorted(set(product(sorted(nums), repeat=m)))]
 
 
-solution()
+# solution()
